4aa885ea52371756cd3af24ed60834be8da152b2
Vishesh <master.vishesh95@gmail.com>
2024-06-03 00:04:38 +0530

    first commit

---
+++ src/dummyData/emplyo.js
@@ -0,0 +1,9 @@
+export const empUserData =  [ {
+    id:28,
+    first_name:"Pables",
+    last_name:"Das",
+    email:"Pables45@gmail.com",
+    company:"Birlasoft",
+    createdAt:'2024-01-16 14:18:31.097+05:30',
+    updatedAt:'2024-01-16 14:18:31.097+05:30'    
+}]
